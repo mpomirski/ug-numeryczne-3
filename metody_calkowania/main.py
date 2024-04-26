255ba6ce352f87b5c2c127c6682e75916d0daab4
mpomirski <michal.pomirski01@gmail.com>
2024-04-26 09:57:49 +0200

    Metoda trapezow

--- metody_calkowania/main.py
+++ metody_calkowania/main.py
@@ -1,52 +1,86 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from typing import Callable
 
 
 class Integral:
-    def __init__(self, f: callable, left: float, right: float):
-        self.f = f
-        self.left = left
-        self.right = right
+    def __init__(self, f: Callable[[np.float64], np.float64], left: float, right: float) -> None:
+        self.f: Callable[[np.float64], np.float64] = f
+        self.left: float = left
+        self.right: float = right
 
-    
-    def calculate(self, x: float) -> float:
+    def calculate(self, x: np.float64) -> np.float64:
         return self.f(x)
-    
+
     def __repr__(self) -> str:
         return f"Integral({self.f} from {self.left} to {self.right})"
-    
-    def plot(self) -> None:
-        x = np.linspace(self.left, self.right, 100)
-        y = self.f(x)
-        plt.plot(x, y)
-        plt.show()
 
-    def rectangleMethod(self, n: int) -> float:
-        dx = (self.right - self.left) / n
-        result = 0
-        for i in range(n):
-            x = self.left + i * dx
-            result += self.f(x) * dx
-        return result
-    
-    def plotRectangleMethod(self, n: int) -> None:
-        x = np.linspace(self.left, self.right, 100)
-        y = self.f(x)
+    def plot(self) -> None:
+        x: np.ndarray[np.float64, np.dtype] = np.linspace(
+            self.left, self.right, 100)
+        y: np.ndarray[np.float64, np.dtype] = self.f(x)  # type: ignore
         plt.plot(x, y)
-        dx = (self.right - self.left) / n
+
+    def rectangleMethod(self, n: np.int32) -> np.float64:
+        dx: np.float64 = np.float64(self.right - self.left) / n
+        result: np.float64 = np.float64(0)
         for i in range(n):
-            x = self.left + i * dx + dx / 2
-            plt.plot(x, self.f(x), 'o', color="red")
-            plt.bar(x - 0.5 * dx, self.f(x), dx, alpha=0.5, color="red", edgecolor="black", align='edge')
-        plt.show()
+            x: np.float64 = self.left + i * dx
+            result: np.float64 = result + self.f(x) * dx
+        return result
+
+    def plotRectangleMethod(self, n: np.int32) -> None:
+        xarr: np.ndarray[np.float64, np.dtype] = np.linspace(
+            self.left, self.right, 100)
+        y: np.float64 = self.f(xarr)  # type: ignore
+        plt.plot(xarr, y)
+        dx: np.float64 = np.float64(self.right - self.left) / n
+        for i in range(n):
+            x: np.float64 = self.left + i * dx + dx / 2
+            # Dots
+            # plt.plot(x, self.f(x), 'o', color="red")
+            plt.bar(x - 0.5 * dx, self.f(x), dx, alpha=0.5,
+                    color="red", edgecolor="black", align='edge')
+
+    def trapezoidMethod(self, n: np.int32) -> np.float64:
+        dx: np.float64 = np.float64(self.right - self.left) / n
+        result: np.float64 = np.float64(0)
+        for i in range(n):
+            x0: np.float64 = self.left + i * dx
+            x1: np.float64 = self.left + (i + 1) * dx
+            result: np.float64 = result + (self.f(x0) + self.f(x1)) * dx / 2
+        return result
+
+    def plotTrapezoidMethod(self, n: np.int32) -> None:
+        xarr: np.ndarray[np.float64, np.dtype] = np.linspace(
+            self.left, self.right, 100)
+        y: np.float64 = self.f(xarr)  # type: ignore
+        plt.plot(xarr, y)
+        dx: np.float64 = np.float64(self.right - self.left) / n
+        for i in range(n):
+            x0: np.float64 = self.left + i * dx
+            x1: np.float64 = self.left + (i + 1) * dx
+            plt.plot([x0, x1], [self.f(x0), self.f(x1)], color="red")
+            plt.fill_between([x0, x1], [self.f(x0), self.f(x1)],
+                             alpha=0.5, color="red")
 
 
 def main() -> None:
-    def f(x):
-        return np.sin(x)
-    integral = Integral(f, -np.pi, +np.pi)
-    print(integral.rectangleMethod(10))
-    integral.plotRectangleMethod(10)
+    steps: np.int32 = np.int32(7)
+
+    def f(x: np.float64) -> np.float64:
+        return x
+    integral = Integral(f, -4, +4)
+
+    plt.subplot(3, 1, 1)
+    print(integral.rectangleMethod(steps))
+    integral.plotRectangleMethod(steps)
+
+    plt.subplot(3, 1, 2)
+    print(integral.trapezoidMethod(steps))
+    integral.plotTrapezoidMethod(steps)
+    plt.show()
+
 
 if __name__ == "__main__":
     main()
